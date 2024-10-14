e2a5e8cae4b8ed0d52ad521cae6c6810c5d9d0f0
jonathan tamir <jonathan.tamir@ericom.com>
2024-10-14 11:01:31 +0300

    das

--- showCefclicks.js
+++ showCefclicks.js
@@ -1,6 +1,21 @@
-document.addEventListener('touchmove', e => {
+document.addEventListener('touchstart', e => {
     const t = e.touches[0];
     const d = document.createElement('div');
-    d.style.cssText = `width:5px; height:5px; background:red; position:absolute; left:${t.pageX}px; top:${t.pageY}px; border-radius:50%; z-index: 99999;`;
+    d.style.position = 'absolute';
+    d.style.width = '10px';
+    d.style.height = '10px';
+    d.style.backgroundColor = 'red';
+    d.style.borderRadius = '50%';
+    d.style.top = `${t.pageY}px`;
+    d.style.left = `${t.pageX}px`;
+    d.style.transform = 'translate(-50%, -50%)';
+    d.style.zIndex = 800; 
     document.body.appendChild(d);
+
+    setTimeout(() => {
+        d.remove();
+        console.log('Marker removed.');
+      }, 60 * 1000); 
+
+
 });
